ace5fd056969fead4c29cc391a242c8f05690946
salmanaltamimi <a_s_salman99@hotmail.com>
2019-10-03 21:34:24 +0400

    Feature B Added by Ahmed Kanaani

--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -2,12 +2,20 @@ package helloworld;
 
 public class HelloWorld {
 
-	public void addFeature() {
+	public static void addFeatureB() {
+
+		System.out.println("Feature B added by Ahmed Kanaani");
+
 	}
 	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 System.out.println("Hello World!");
+
+addFeatureB();
+
+
 	}
 
 }
